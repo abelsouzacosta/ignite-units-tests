e804e8d4d232d5a399fc758c24d9a5dd67dfdda1
Abel Souza <abelsouzacosta@gmail.com>
2022-03-17 06:54:01 -0300

    adding test: create user use case
    Should not be able to create an user with an email already taken

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,4 +1,5 @@
 import { InMemoryUsersRepository } from "../../repositories/in-memory/InMemoryUsersRepository"
+import { CreateUserError } from "./CreateUserError";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
 let repository: InMemoryUsersRepository;
@@ -22,5 +23,18 @@ describe("Create User Use Case", () => {
     let userCreated = await repository.findByEmail(user.email);
 
     expect(userCreated).toHaveProperty("id");
+  });
+
+  it("Should not be able to create an user with an email already taken", () => {
+    expect(async () => {
+      let user = {
+        name: "Abel Souza Costa Junior",
+        email: "abelsouzacosta@gmail.com",
+        password: "123456"
+      }
+
+      await create.execute(user);
+      await create.execute(user);
+    }).rejects.toBeInstanceOf(CreateUserError)
   })
 })
